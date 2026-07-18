835fb8635fb5c6d195dfca04432affde9f35dab0
exponent <baejisu0910@gmail.com>
2026-07-19 02:49:34 +0900

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -2,6 +2,5 @@
 # 1. 파이썬 의존성 설치
 pip install -r requirements.txt
 
-# 2. 브라우저 및 시스템 종속성 설치 (필수!)
-playwright install
-playwright install-deps
+# 2. 브라우저만 설치 (install-deps는 제거!)
+playwright install chromium
